1d20038180aa1dd8efb889dc050bb49a9728ead0
alex1feu17 <alexyd@hotmail.ca>
2020-04-08 14:04:29 -0400

    Modele3.2

---
+++ Inventaire/ViewModels/Invoice.cs
@@ -0,0 +1,30 @@
+﻿using app_models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BillingManagement.UI.ViewModels
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+
+        public DateTime CreationDateTime { get; }
+
+        Customer Customer { get; set; }
+
+        public double SubTotal { get; set; }
+
+        public double FedTax { get; set; }
+
+        public double ProvTax { get; set; }
+
+        public double Total { get; set; }
+
+        public Invoice(Customer c)
+        {
+            CreationDateTime = new DateTime(2020, 4, 8, 1, 37, 0);
+            c = Customer;
+        }
+    }
+}
